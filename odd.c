d8cf599bf4da915dcff3bee9e049ba955b07302a
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2017-08-05 16:36:00 +0530

    odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the a value");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the num is even");
+}
+else
+{
+printf("the num is odd");
+}
+getch();
+}

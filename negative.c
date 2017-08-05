57bb16a6536837974acc60f9f95056edfa73cd1a
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2017-08-05 16:04:08 +0530

    negative.c

---
+++ negative.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the a value");
+scanf("%d",&a);
+if(a<0)
+{
+printf("a is negative");
+}
+else if(a=0)
+{
+printf("a is equal to zero");
+}
+else
+{
+printf("a is positive");
+}
+getch();
+}

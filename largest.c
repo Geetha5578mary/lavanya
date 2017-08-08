addd0992dfd6e1b3960c21c735ac7dc52e570bc4
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2017-08-08 15:55:33 +0530

    largest.c

---
+++ largest.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void maion()
+{
+int a,b,c;
+printf("enter the values");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+printf("a is greater");
+}
+else if(b>c)
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}
+getch();
+)
+
+
+
+

c201c7d4a6df257dd6227c021a47040b1c203e79
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2017-08-22 15:14:21 +0530

    char.c

---
+++ char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("enter the character");
+scanf("%s",c);
+if(c>='a'&&c<='z')
+{
+printf("it is character");
+}
+else
+{
+printf("it is not character");
+}
+getch();
+}

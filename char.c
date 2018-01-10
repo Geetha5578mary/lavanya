f8ecf5c6a38c70faf585eb039f77f18019ab8bb1
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-01-10 15:51:31 +0530

    char.c

--- char.c
+++ char.c
@@ -4,13 +4,13 @@ void main()
 char c;
 printf("enter the character");
 scanf("%s",c);
-if(c>='a'&&c<='z')
+if(c<='a'&&c>='z')
 {
-printf("it is character");
+printf("alphabet");
 }
 else
 {
-printf("it is not character");
+printf("not alphabet");
 }
 getch();
 }

ed8e0c64fef18931652d4dbeffae9253076bdd59
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-01-22 16:16:45 +0530

    prime.c

--- prime.c
+++ prime.c
@@ -1,16 +1,25 @@
-#include<stdio.h>
-int main()
-{
-printf("enter the i");
-scanf("%d",&i);
-for(i=2;i<n;i++)
-if(n%i==0)
-{
-printf("is prime");
+#include <stdio.h>
+ 
+int main(void) {
+    int n, i, flag = 0;
+ 
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+ 
+    for(i=2; i<=n/2; ++i)
+    {
+ 
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+ 
+    if (flag==0)
+        printf("%d is a prime number.",n);
+    else
+        printf("%d is not a prime number.",n);
+ 
+    return 0;
 }
-else
-{
-printf("no prime");
-}
-}
-

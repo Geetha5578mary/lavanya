788d195309215c46610eb3153385b2e7d056b874
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-01-31 16:36:29 +0530

    20.c

---
+++ 20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+int n,a,b,c,d,e;
+printf("enter the number");
+scanf("%d",&n);
+a=n*1;
+b=n*2;
+c=n*3;
+d=n*4;
+e=n*5;
+printf("the result is  %d\t%d\t%d\t%d\t%d\t",a,b,c,d,e);
+	return 0;
+}

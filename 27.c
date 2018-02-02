0e4192ac6382ac283120372ae42c0f3650ad99ce
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-02-02 15:53:40 +0530

    27.c

---
+++ 27.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	printf("\nenter the n");
+	scanf("%d",&n);
+	if(n>=0)
+	{
+		printf(" \nnumeric");
+	}
+	else
+	{
+	printf("not numeric");
+	}
+	return 0;
+}

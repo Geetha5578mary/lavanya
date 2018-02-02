032f8dbb065228486452f094de5471cd694b1077
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-02-02 16:52:47 +0530

    22.c

---
+++ 22.c
@@ -0,0 +1,18 @@
+nclude <stdio.h>
+
+int main(void) {
+int a,b[35],i,t;
+printf("enter the size\n");
+scanf("%d",&a);
+printf("array elements\n");
+for(i=0;i<=a;i++)
+{
+	scanf("%d",&b[35]);
+}
+if(b[35] > a)
+{
+	t=b[50];
+}
+printf("%d\n",t);
+	return 0;
+}

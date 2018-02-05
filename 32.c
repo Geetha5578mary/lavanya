89986d8f4bda63524f2beaea5ed8df7c1fa52d47
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-02-05 16:26:35 +0530

    32.c

--- 32.c
+++ 32.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+
+int main(void) {
+char a[50];
+	int n,i,count=1;
+	printf("\n enter the values");
+	gets(a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]!=' ' && a[i+1]==' ')
+			count=count+1;
+	}
+	printf("\n%d",count);
+	return 0;
+}

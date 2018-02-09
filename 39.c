6aa545113ad39597facf0754a4aa341f384a9e64
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-02-09 15:49:28 +0530

    39.c

---
+++ 39.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+int a=10,b[35],i,t;
+printf("array elements\n");
+for(i=0;i<=a;i++)
+{
+	scanf("%d",&b[35]);
+}
+if(b[35] > a)
+{
+	t=b[35];
+}
+printf("\nthe maximum element is%d",t);
+	return 0;
+}

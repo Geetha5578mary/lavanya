368867823093334661bd3534a403a842cb5d866a
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-02-12 16:24:56 +0530

    45.c

---
+++ 45.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,count=0;
+	printf("enter the a");
+	scanf("%d",&a);
+    while(a!=0)
+    {
+        a=a/10;
+        count++;
+        
+    }
+    printf("%d",count);
+	return 0;
+}

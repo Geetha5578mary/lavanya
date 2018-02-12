0e4a2220dee85f53df71d6492006156495b01132
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-02-12 16:56:46 +0530

    48.c

---
+++ 48.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i,avg,tot=0;
+	printf("enter the size\n");
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+	printf("%d\n",i);
+	tot=tot+i;
+	}
+	avg=tot/n;
+	printf("the avgerage is %d\n",avg);
+	return 0;
+}

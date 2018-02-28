0ce88b9225b4e26164c3c136df22bfa25df9afb7
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-02-28 16:34:17 +0530

    72.C

---
+++ 72.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+char s[100];
+int l,i,count=0;
+scanf("%s",s);
+l=strlen(s);
+for(i=0;i<l;i++)
+   {
+if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+count++;
+ else
+      continue;
+   }
+   if(count>0)
+   printf("yes");
+   else
+   printf("no");
+    getch();
+    }

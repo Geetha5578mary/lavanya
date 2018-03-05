6a2fea6bf085a5e08be081981877a1cf7b457e93
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-03-05 16:40:33 +0530

    75.c

---
+++ 75.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char s[100];
+int l,i,t;
+clrscr();
+scanf("%s",s);
+l=strlen(s);
+t=l/2;
+if(l%2==0)
+{
+s[t-1]='*';
+s[t]='*';
+printf("%s",s);
+}
+else
+{
+s[t]='*';
+printf("%s",s);
+ }
+getch();
+}

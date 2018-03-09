cb3a8d974f2f63b6751aac53dbc18b88c9bb45a3
lavanya4 <30549719+lavanya4@users.noreply.github.com>
2018-03-09 16:02:50 +0530

    82.c

---
+++ 82.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a,b;
+double c;
+clrscr();
+printf("enter the length and breadth");
+scanf("%f%f",&a,&b);
+c=a*b;
+printf("%f\n",c);
+getch();
+}
